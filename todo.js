863119597b2166c725a463f35920775175d3c4d2
Magdalena Kita <magdalenakita96@gmail.com>
2024-06-10 11:36:04 +0200

    Added addEventListener for keypress. Changed createTask function. Added addTask, saveTask, getTask and loadTask from Local Storage

--- todo.js
+++ todo.js
@@ -4,36 +4,79 @@ const todoList = document.getElementById('todoList');
 
 todoForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    getNewTask()
+});
+
+todoForm.addEventListener('keypress', (event) => {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+        getNewTask()
+    }
+});
+
+document.addEventListener('DOMContentLoaded', loadTaskFromLocalStorage);
+
+function getNewTask() {
     const newTask = todoInput.value.trim();  // trim usuwa puste znaki np. spację na końcu
 
     if(newTask === '') {
         alert('Wpisz zadanie');
         return; // zatrzymuje dodanie pustego pola do listy
     }
-    
-    addTask(newTask);
-});
 
-function addTask(newTask) {  
+    addTask(newTask);
+}
+
+function addTask(newTask) {     
     createTask(newTask);
-    todoInput.value = ''; // wyczyszczenie inputa   
+    addTaskToLocalStorage(newTask);    
+
+    todoInput.value = ''; // wyczyszczenie inputa       
 }
 
 function createTask(newTask) {
     const task = document.createElement('li');
-    const taskDiv = document.createElement('div');
-    const taskSpan = document.createElement('span');
-    const checkbox = document.createElement('input');
-    const deleteButton = document.createElement('button');
+    task.className = 'todoItem';
 
+    const taskDiv = document.createElement('div');
+
+    const taskSpan = document.createElement('span');
     taskSpan.textContent = newTask;
+
+    const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    deleteButton.textContent = 'Delete';
+
+    // const deleteButton = document.createElement('button');
+    // deleteButton.textContent = 'Usuń';
+    // deleteButton.className = 'deleteTaskButton';
 
     taskDiv.appendChild(checkbox);
     taskDiv.appendChild(taskSpan);
     task.appendChild(taskDiv);
-    task.appendChild(deleteButton);
+    // task.appendChild(deleteButton);
     todoList.appendChild(task);
-    
+
+    return task;
+}
+
+function addTaskToLocalStorage(newTask) {
+    const tasksArray = getTaskFromLocalStorage();
+    tasksArray.push(newTask);
+    saveTaskToLocalStorage(tasksArray);    
+}
+
+function saveTaskToLocalStorage(tasksArray) {
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+}
+
+function getTaskFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+}
+
+function loadTaskFromLocalStorage() {
+    const tasksArray = getTaskFromLocalStorage();
+    todoList.innerHTML = '';
+    tasksArray.forEach(task => {
+        createTask(task);
+    });
 }
