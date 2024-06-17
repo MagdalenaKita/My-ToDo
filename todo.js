c7c0cab3597e73295ce95b720083efc27d36c163
Magdalena Kita <magdalenakita96@gmail.com>
2024-06-17 21:36:31 +0200

    Changed structure

--- todo.js
+++ todo.js
@@ -1,122 +1,251 @@
-// Pobranie potrzebnych elementów z HTML i zapisanie ich do zmiennych, żeby potem ich używać w programie
 const todoForm = document.getElementById('todoForm');
 const todoInput = document.getElementById('todoInput');
-// const todoList = document.getElementById('todoList');
-const todoGroup = document.getElementById('groups');
-const todoListImportant = document.getElementById('todoListImportant');
-const todoListNeutral = document.getElementById('todoListNeutral');
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];  //ta tablica służy mi do operacji w localStorage do zapisywania wszystkich zadań w jednej tablicy, pomocne to jest przy zaznaczaniu wszystkich zadań
+
+const groupForm = document.getElementById('groupForm');
+const groupInput = document.getElementById('groupInput');
+const selectGroups = document.getElementById('selectGroups');
+const listGroup = document.getElementById('listGroup');
+
 const checkboxCheckAllTasks = document.getElementById('checkAllTasks');
 
-// od razu nasłuchuje na załadowanie zadań z localStorage
-document.addEventListener('DOMContentLoaded', loadTasks);
+const groups = JSON.parse(localStorage.getItem('groups')) || [];
 
-// tu "pobieram" moj formularz i nasłuchuje, czy kliknięto "dodaj" lub wciśnięto enter
-todoForm.addEventListener('submit', (event) => {
-    event.preventDefault(); // zatrzymuje domyślną operację, czyli zatrzymuje wysyłanie formularza
-    getNewTask(); // wywołanie funkcji, która pobierze mi wpisane zadanie
-});
 
-// Tutaj nasłuchuja na zmianę statusu checkboxa, jest tu uzyta tablica tasks z localStorage i dla każdego zadania "task" z tablicy "tasks" zrób to samo, czyli zmień status checkboxa
-checkboxCheckAllTasks.addEventListener("change", () => {
-    tasks.forEach(task => { 
-        checkboxCheckAllTasks.checked ? task.completed = true : task.completed = false;
-        saveTasks();
-        renderTasks();
-    })
-});
+const getNewGroup = (event) => {
+    event.preventDefault();
+    const newGroup = groupInput.value.trim();
 
-// pobieranie nowego zadania i grupy
-function getNewTask() {  
-    const newTask = todoInput.value.trim();  // trim usuwa puste znaki np. spację na końcu
-    const selectedGroup = todoGroup.value;
+    if(newGroup === '') {
+        alert('Wpisz nazwę grupy');
+        return; // zatrzymuje dodanie pustego pola do listy
+    }
+
+    let group = {
+        groupName: newGroup,
+        tasks: []
+    }
+
+    createGroup(group);
+    addGroup(group);
+    groupInput.value = '';
+}
+
+const createGroup = (group) => {
+    const createdGroup = document.createElement('li');
+    const headerGroup = document.createElement('h3');
+    const groupTaskList = document.createElement('ul');
+
+    headerGroup.textContent = group.groupName;
+    groupTaskList.className = 'listTask';
+
+    group.tasks.forEach(taskElement => {
+        // const index = group.tasks.length - 1;
+        const li = createTask(taskElement);
+        groupTaskList.appendChild(li);
+    });
+
+    createdGroup.appendChild(headerGroup);
+    createdGroup.appendChild(groupTaskList);
+    listGroup.appendChild(createdGroup);
+}
+
+const addGroup = (group) => {
+    groups.push(group);
+    saveGroup();
+    loadGroups();
+}
+
+const loadGroupsToSelect = () => {   
+    selectGroups.innerHTML = '<option value="">Wybierz grupę</option>'; 
+    groups.forEach(group => {
+        const groupOption = document.createElement('option');
+        groupOption.value = group.groupName;
+        groupOption.textContent = group.groupName;
+        selectGroups.appendChild(groupOption);
+    });    
+}
+
+const getNewTask = (event) => {
+    event.preventDefault();
+    const newTask = todoInput.value.trim();
 
     if(newTask === '') {
         alert('Wpisz zadanie');
         return; // zatrzymuje dodanie pustego pola do listy
     }
 
-    // stworzenie zadania jako obiektu, który będzie miał 3 parametry
+    if(groups.length == 0) {
+        alert('Proszę wpisać najpierw grupę');
+        return;
+    }
+
+    if(selectGroups.value === 'Wybierz grupę') {
+        alert('Proszę wybrać grupę');
+        return;
+    }
+
     let task = {
         text: newTask,
-        completed: false,
-        group: selectedGroup
+        completed: false
     }
-
-    createTask(task); // najpierw tworzymy nasze li
-    addTask(task); // a potem dodajemy nasze zadanie
-    todoInput.value = ''; // wyczyszczenie inputa       
+        
+    createTask(task);
+    addTask(task);
+    todoInput.value = ''; 
 }
+
+const createTask = (task) => {
+
+    // const indexGroup = groups.findIndex(group => group.groupName);
+
+    // const indexTask = groups[indexGroup].tasks.length - 1;
+
+    const todoTask = document.createElement('li');
+    todoTask.className = task.completed ? 'styleComplete' : '';
+
+    const todoTaskDiv = document.createElement('div');
+
+    const todoTaskSpan = document.createElement('span');
+    todoTaskSpan.textContent = task.text;
+
+    const todoTaskCheckbox = document.createElement('input');
+    todoTaskCheckbox.type = 'checkbox';
+    todoTaskCheckbox.checked = task.completed;
+    
+    todoTaskCheckbox.addEventListener('change', () => {
+        task.completed = !task.completed;
+        saveGroup();
+        loadGroups();
+    })
+
+    const deleteTaskButton = document.createElement('button');
+    deleteTaskButton.textContent = 'Usuń';
+    deleteTaskButton.className = 'deleteTaskButton';
+
+    deleteTaskButton.addEventListener('click', () => {
+        // groups[indexGroup].tasks.splice(indexTask, 1);
+        // console.log("Usuniecie grupy", indexGroup);
+        // console.log("Usuniecie zadania", indexTask);
+        // saveGroup();
+        // loadGroups();
+    })
+
+    todoTaskDiv.appendChild(todoTaskCheckbox);
+    todoTaskDiv.appendChild(todoTaskSpan);
+    todoTask.appendChild(todoTaskDiv);
+    todoTask.appendChild(deleteTaskButton);
+
+    return todoTask;
+}
+
+const addTask = (task) => {
+    const selectGroupName = selectGroups.value;
+    const indexGroupToSaveTask = groups.findIndex(group => selectGroupName === group.groupName);
+    groups[indexGroupToSaveTask].tasks.push(task);
+    // const indexTask = groups[indexGroupToSaveTask].tasks.length - 1;
+    
+    saveGroup();
+    loadGroups();
+}
+
+const saveGroup = () => {
+    localStorage.setItem('groups', JSON.stringify(groups));
+}
+
+const loadGroups = () => {
+    listGroup.innerHTML = '';
+    loadGroupsToSelect();
+    groups.forEach((group) => {
+        createGroup(group);
+    })
+}
+
+const markAllTasks = () => {
+    groups.forEach(group => {
+        group.tasks.forEach(task => {
+            checkboxCheckAllTasks.checked ? task.completed = true : task.completed = false;
+            saveGroup();
+            loadGroups();
+        })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', loadGroups);
+groupForm.addEventListener('submit', getNewGroup);
+todoForm.addEventListener('submit', getNewTask);
+checkboxCheckAllTasks.addEventListener("change", markAllTasks);
+
+
 
 // towrzenie zadania, czyli dodawanie odpowiednich elementów z HTML i klas. 
-function createTask(newTask, indexTask) {
-    const task = document.createElement('li');
-    task.className = 'todoItem';
-    task.className = newTask.completed ? 'styleComplete' : ''; // newtask to jest to samo co task z 40 linii, a task to jest to co pobralismy w linii 53 (pozmieniać potem te nazwy, bo może się mylić)
+// function createTask(newTask, indexTask) {
+//     const task = document.createElement('li');
+//     task.className = 'todoItem';
+//     task.className = newTask.completed ? 'styleComplete' : ''; // newtask to jest to samo co task z 40 linii, a task to jest to co pobralismy w linii 53 (pozmieniać potem te nazwy, bo może się mylić)
 
-    const taskDiv = document.createElement('div');
+//     const taskDiv = document.createElement('div');
 
-    const taskSpan = document.createElement('span');
-    taskSpan.textContent = newTask.text;
+//     const taskSpan = document.createElement('span');
+//     taskSpan.textContent = newTask.text;
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = newTask.completed;
+//     const checkbox = document.createElement('input');
+//     checkbox.type = 'checkbox';
+//     checkbox.checked = newTask.completed;
     
-    checkbox.addEventListener('change', () => {
-        newTask.completed = !newTask.completed;
-        saveTasks();
-        renderTasks(indexTask);
-    })
+//     checkbox.addEventListener('change', () => {
+//         newTask.completed = !newTask.completed;
+//         saveTasks();
+//         renderTasks(indexTask);
+//     })
 
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Usuń';
-    deleteButton.className = 'deleteTaskButton';
-    deleteButton.addEventListener('click', () => {
-        tasks.splice(indexTask, 1);        
-        saveTasks();
-        renderTasks(indexTask);
-    })
+//     const deleteButton = document.createElement('button');
+//     deleteButton.textContent = 'Usuń';
+//     deleteButton.className = 'deleteTaskButton';
+//     deleteButton.addEventListener('click', () => {
+//         tasks.splice(indexTask, 1);        
+//         saveTasks();
+//         renderTasks(indexTask);
+//     })
 
-    taskDiv.appendChild(checkbox);
-    taskDiv.appendChild(taskSpan);
-    task.appendChild(taskDiv);
-    task.appendChild(deleteButton);    
+//     taskDiv.appendChild(checkbox);
+//     taskDiv.appendChild(taskSpan);
+//     task.appendChild(taskDiv);
+//     task.appendChild(deleteButton);    
     
     // todoList.appendChild(task);
-    if(newTask.group === 'important') {
-        todoListImportant.appendChild(task);
-    } else {
-        todoListNeutral.appendChild(task);
-    }
-}
+    // if(newTask.group === 'important') {
+    //     todoListImportant.appendChild(task);
+    // } else {
+    //     todoListNeutral.appendChild(task);
+    // }
+// }
 
 // dodajemy zadanie do tablicy tasks
-function addTask(task) {   
-    tasks.push(task);
-    const indexTask = tasks.length - 1;
-    saveTasks();
-    renderTasks(indexTask);
-}
+// function addTask(task) {   
+//     tasks.push(task);
+//     const indexTask = tasks.length - 1;
+//     saveTasks();
+//     renderTasks(indexTask);
+// }
 
 // zapisywanie tablicy tasks do localStorage
-function saveTasks() {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+// function saveTasks() {
+//     localStorage.setItem('tasks', JSON.stringify(tasks));
+// }
 
-function loadTasks() {
-    renderTasks();
-}
+// function loadTasks() {
+//     renderTasks();
+// }
 
 // wyrenderowanie zadań na ekranie
-function renderTasks() {
-    // todoList.innerHTML = '';
-    todoListImportant.innerHTML = '';
-    todoListNeutral.innerHTML = '';
-    tasks.forEach((task, indexTask) => {
-        createTask(task, indexTask);
-    })
-}
+// function renderTasks() {
+//     // todoList.innerHTML = '';
+//     todoListImportant.innerHTML = '';
+//     todoListNeutral.innerHTML = '';
+//     tasks.forEach((task, indexTask) => {
+//         createTask(task, indexTask);
+//     })
+// }
 
 
 // naprawa programu
